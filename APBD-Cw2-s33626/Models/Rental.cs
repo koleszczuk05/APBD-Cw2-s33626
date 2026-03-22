706f2aa407a3bb64394317935e8f1983fb235b1f
Krzysztof Oleszczuk <koleszczuk05@gmail.com>
2026-03-22 21:23:19 +0100

    added class rental with a basic check if end date is smaller than start date

---
+++ APBD-Cw2-s33626/Models/Rental.cs
@@ -0,0 +1,14 @@
+﻿namespace APBD_Cw2_s33626.Models;
+
+public class Rental(Device device, User user, DateTime startDate, DateTime endDate)
+{
+    private static int _rentalId = 1;
+    public DateTime? ActDate { get; set; } = null;
+    public int Id { get; set; } = _rentalId++;
+    public Device Device { get; set; } = device;
+    public User User { get; set; } = user;
+    public DateTime StartDate { get; set; } = startDate >= endDate ? throw new ArgumentException("Invalid time range") : startDate;
+    public DateTime EndDate { get; set; } = endDate;
+    public double Penalty { get; set; } = 0;
+    
+}
