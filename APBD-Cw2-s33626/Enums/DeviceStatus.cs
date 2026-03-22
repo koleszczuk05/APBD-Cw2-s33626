0dcef5cdf879845f0cb3cc6eadfa20ca8357d42d
Krzysztof Oleszczuk <koleszczuk05@gmail.com>
2026-03-22 17:58:44 +0100

    Added enums and abstract base class for devices

---
+++ APBD-Cw2-s33626/Enums/DeviceStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace APBD_Cw2_s33626.Enums;
+
+public enum DeviceStatus
+{
+    Available,
+    Rented,
+    Damaged
+}
---
+++ APBD-Cw2-s33626/Models/Device.cs
@@ -0,0 +1,13 @@
+﻿namespace APBD_Cw2_s33626.Models;
+using APBD_Cw2_s33626.Enums;
+
+public abstract class Device(string name, double dayCost)
+{
+    private static int idDevice = 1;
+
+    public int Id { get; } =  idDevice++;
+    public string Name { get; set; } = name;
+    public string Description { get; set; } = "";
+    public double DayCost { get; set; } = dayCost;
+    public DeviceStatus Status { get; set; } = DeviceStatus.Available;
+}
