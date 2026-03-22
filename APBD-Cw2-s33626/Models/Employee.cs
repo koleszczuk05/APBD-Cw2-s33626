9617d1640ce4810059feb9f982b463e45cde11c2
Krzysztof Oleszczuk <koleszczuk05@gmail.com>
2026-03-22 20:35:51 +0100

    added classes student and employee that extends user

---
+++ APBD-Cw2-s33626/Models/Employee.cs
@@ -0,0 +1,7 @@
+﻿namespace APBD_Cw2_s33626.Models;
+
+public class Employee(string firstName, string lastName,double salary) : User(firstName, lastName)
+{
+    public override int MaxItems { get; } = 5;
+    public double Salary { get; set; } = salary;
+}
---
+++ APBD-Cw2-s33626/Models/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace APBD_Cw2_s33626.Models;
+
+public class Student(string firstName, string lastName, string sNumber) : User(firstName, lastName)
+{
+    public override int MaxItems { get; } = 2;
+    public string SNumber { get; set; } = sNumber;
+}
