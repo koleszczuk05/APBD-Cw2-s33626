88f3a5d2f8b3c5f888ff2ee64780913ab00a75fc
Krzysztof Oleszczuk <koleszczuk05@gmail.com>
2026-03-23 01:03:09 +0100

    created a databse for rentals, users and devices which will be needed in service classes

---
+++ APBD-Cw2-s33626/Data/DataBase.cs
@@ -0,0 +1,10 @@
+﻿using APBD_Cw2_s33626.Models;
+
+namespace APBD_Cw2_s33626.Data;
+
+public class DataBase()
+{
+    public List<Rental> Rentals { get; set; } = [];
+    public List<User> Users { get; set; } = [];
+    public List<Device> Devices { get; set; } = [];
+}
---
+++ APBD-Cw2-s33626/Services/Rentals/IRentalService.cs
@@ -0,0 +1,6 @@
+﻿namespace APBD_Cw2_s33626.Services.Rentals;
+
+public interface IRentalService
+{
+    
+}
