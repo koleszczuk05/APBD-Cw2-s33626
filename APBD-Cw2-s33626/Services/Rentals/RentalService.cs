3adcebc16be62acf63e5a49b3861adf45ff73c2e
Krzysztof Oleszczuk <koleszczuk05@gmail.com>
2026-03-23 15:37:31 +0100

    Implemented RentDevice in RentalService and modified UserService

---
+++ APBD-Cw2-s33626/Services/Rentals/RentalService.cs
@@ -0,0 +1,74 @@
+﻿using APBD_Cw2_s33626.Data;
+using APBD_Cw2_s33626.Enums;
+using APBD_Cw2_s33626.Models;
+
+namespace APBD_Cw2_s33626.Services.Rentals;
+
+public class RentalService(DataBase dataBase) : IRentalService 
+{
+    public void RentDevice(int userId, int deviceId, DateTime startDate, DateTime endDate)
+    {
+        var countRentals = 0;
+        User? userAct = null;
+        Device? deviceAct = null;
+        foreach (var user in dataBase.Users)
+        {
+            if (user.Id == userId)
+            {
+                userAct = user;
+            }
+        }
+        
+        foreach (var rental in dataBase.Rentals)
+        {
+            if (rental.User.Id == userId && rental.ActDate == null)
+            {
+                countRentals++;
+            }
+        }
+
+        foreach (var device in dataBase.Devices)
+        {
+            if (device.Id == deviceId)
+            {
+                deviceAct = device;
+            }
+        }
+
+        if (deviceAct == null || userAct == null)
+        {
+            throw new ArgumentException("Device or User Not Found");
+        }
+
+        if (deviceAct.Status != DeviceStatus.Available)
+        {
+            throw new ArgumentException("Device not available");
+        }
+
+        if (countRentals == userAct.MaxItems)
+        {
+            throw new ArgumentException($"User {userId} is already renting max number of devices");
+        }
+        else if (userAct.MaxItems > countRentals)
+        {
+            var rental = new Rental(deviceAct, userAct, startDate, endDate);
+            deviceAct.Status = DeviceStatus.Rented;
+            dataBase.Rentals.Add(rental);
+        }
+    }
+
+    public void ReturnDevice(int userId, int deviceId, DateTime returnDate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ListOverDueDevices()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CreateRentingRaport()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ APBD-Cw2-s33626/Services/Users/UserService.cs
@@ -0,0 +1,30 @@
+﻿using APBD_Cw2_s33626.Data;
+using APBD_Cw2_s33626.Models;
+
+namespace APBD_Cw2_s33626.Services.Users;
+
+public class UserService(DataBase dataBase) : IUserService
+{
+    public void AddUser(User user)
+    {
+        if (!dataBase.Users.Contains(user))
+        {
+            dataBase.Users.Add(user);
+        }
+        else
+        {
+            throw new ArgumentException("User already exists");
+        }
+    }
+
+    public void ListUserRentals(int userId)
+    {
+        foreach (var rental in dataBase.Rentals)
+        {
+            if (rental.User.Id == userId && rental.ActDate == null)
+            {
+                Console.WriteLine($"{rental.User.Id} - {rental.User.Name} - {rental.Device.Name}");
+            }
+        }
+    }
+}
