b50b93bda4522aee7da57fe97c878dabc0136fb6
Krzysztof Oleszczuk <koleszczuk05@gmail.com>
2026-03-24 03:13:33 +0100

    First version of tests

--- APBD-Cw2-s33626/Program.cs
+++ APBD-Cw2-s33626/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using APBD_Cw2_s33626.Data;
+using APBD_Cw2_s33626.Services.Devices;
+using APBD_Cw2_s33626.Services.Rentals;
+using APBD_Cw2_s33626.Services.Users;
+
+var db = new DataBase();
+var rentalService = new RentalService(db);
+var deviceService = new DeviceService(db);
+var userService = new UserService(db);
